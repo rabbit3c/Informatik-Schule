544a185f258e18440dfd4307bef3cc24e57cf953
Matthieu Cottet <matthieu.cottet@outlook.com>
2024-02-01 16:04:59 +0100

    übungen gelöst

--- Javascript Versuche/array_ubungen.js
+++ Javascript Versuche/array_ubungen.js
@@ -15,4 +15,50 @@ function makeRandomArray(min, max, amount) {
     return randomArray;
 }
 
-console.log(makeRandomArray(0, 70, 100));
+function findMin(array) {
+    let min = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (min > array[i]) min = array[i];
+    }
+    return min;
+}
+
+function shuffleString(string) {
+    let array = string.split("");
+    for (let  i = 0; i < 100; i++) {
+        let j = Math.floor(Math.random() * array.length);
+        let l = Math.floor(Math.random() * array.length);
+        let element1 = array[j];
+        array[j] = array[l];
+        array[l] = element1;
+    }
+    return array.join("");
+}
+
+function carefulMix(string) {
+    let array = string.split("");
+    for (let i = 0; i < 100; i++) {
+        let j = Math.floor(Math.random() * (array.length - 2)) + 1;
+        let l = Math.floor(Math.random() * (array.length - 2)) + 1;
+        let element1 = array[j];
+        array[j] = array[l];
+        array[l] = element1;
+    }
+    return array.join("");
+}
+
+function shuffle(text) {
+    let array = text.split(" ");
+    for (let i = 0; i < array.length; i++) {
+        array[i] = carefulMix(array[i]);
+    }
+    return array.join(" ");
+}
+
+
+let randomArray = makeRandomArray(0, 70, 100);
+console.log(randomArray);
+console.log(findMin(randomArray));
+console.log(shuffleString("Aylin"));
+console.log(carefulMix("Aylin"));
+console.log(shuffle("Arbeite jetzt Aylin endlich. Ich will Piggytype spielen können"))
